1040b03254add060e82dcf765ecd35f6bd19a039
sufuan <abu49539@gmail.com>
2022-02-17 21:59:33 +0600

    err hand

--- script.js
+++ script.js
@@ -1,43 +1,113 @@
 function add() {
-    // get all input value 
-    const foodAmount = parseFloat(document.getElementById('food-expense').value)
-    const rentAmount = parseFloat(document.getElementById('rent-expense').value)
-    const clothAmount = parseFloat(document.getElementById('cloth-expense').value)
-        // add total
-    let sumOfExpenses = foodAmount + rentAmount + clothAmount
-    return sumOfExpenses
+    //     // get all input value
+    const foodInput = document.getElementById('food-expense')
+    const rentInput = document.getElementById('rent-expense')
+    const clothinput = document.getElementById('cloth-expense')
+
+    //    validate
+
+    if ((foodInput.value != "" && foodInput.value > 0) && (rentInput.value != "" && rentInput.value > 0) && (clothinput.value != "" && clothinput.value > 0)) {
+        const foodAmount = parseFloat(foodInput.value)
+        const rentAmount = parseFloat(rentInput.value)
+        const clothAmount = parseFloat(clothinput.value)
+
+
+        let sumOfExpenses = foodAmount + rentAmount + clothAmount
+        console.log(sumOfExpenses);
+        document.getElementById('total-expenses').innerText = sumOfExpenses
+        document.getElementById('err1').innerText = ''
+        return sumOfExpenses
+
+
+
+
+    } else {
+
+        document.getElementById('err1').innerText = 'err'
+    }
+
+
 }
+
 
 
 function calc() {
 
     let sumOfExpenses = add()
 
-    // update balance 
+    let totalBalanceInput = document.getElementById('total-income').value
+    let totalBalanceAmount = parseFloat(totalBalanceInput)
 
-    totalBalanceAmount = parseFloat(document.getElementById('total-income').value)
-    const remainingBalance = totalBalanceAmount - sumOfExpenses;
 
-    document.getElementById('total-expenses').innerText = sumOfExpenses
-    document.getElementById('balance').innerText = remainingBalance
-        // return newBalance;
-    return remainingBalance
+    if (totalBalanceInput != '' && totalBalanceInput > 0) {
+
+        let remainingBalance = totalBalanceAmount - sumOfExpenses;
+        if (totalBalanceAmount < sumOfExpenses) {
+            alert('Money expenses can not be bigger than income')
+            document.getElementById('balance').innerText = 0
+            document.getElementById('total-expenses').innerText = 0
+            document.getElementById('err1').innerText = 'err'
+
+        } else {
+
+            document.getElementById('balance').innerText = remainingBalance
+                //  return remainingBalance
+            document.getElementById('err1').innerText = ''
+        }
+        document.getElementById('err').innerText = ''
+
+    } else {
+        document.getElementById('err').innerText = 'err'
+    }
+
+
+    return totalBalanceAmount
+
+
+
+
+
+
 }
 
 // 
 
 
+//
+
+
+
+
+
+
+
 function calculatePercent() {
     // calulate percentence 
-    let saveParcent = parseFloat(document.getElementById('save-parcent').value)
-    let savePercentVal = saveParcent / 100
-    let savings = savePercentVal * totalBalanceAmount
-    document.getElementById('saving-amount').innerText = savings
+    let totalBalanceAmount = calc()
+    console.log(totalBalanceAmount);
+    let InputParcent = document.getElementById('save-parcent').value
+    if (InputParcent > 0 && InputParcent != '') {
+        InputParcentValue = parseFloat(InputParcent)
+        let savePercentVal = InputParcentValue / 100
+        console.log(savePercentVal);
+        let savings = savePercentVal * totalBalanceAmount
+        document.getElementById('saving-amount').innerText = savings
+        console.log('ok');
 
-    // calculate remaining saving 
-    let remainingBalance = calc()
-    let remainSavings = remainingBalance - savings
-    document.getElementById('remain-amount').innerText = remainSavings
+
+
+        let sumOfExpenses = add()
+        let remainingBalance = totalBalanceAmount - sumOfExpenses
+        let remainSavings = remainingBalance - savings
+        document.getElementById('remain-amount').innerText = remainSavings
+        document.getElementById('err2').innerText = ''
+
+    } else {
+        document.getElementById('err2').innerText = 'err'
+    }
+
+
+
 
 
 }
